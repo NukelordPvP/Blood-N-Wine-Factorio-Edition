4dad501f3cc1a2320d1ed1b768c5b4c2df03bc5f
NukelordPvP <63741267+NukelordPvP@users.noreply.github.com>
2023-07-20 19:20:32 -0700

    0.5 cleanup part 2
    0.5 cleanup part 2

--- Blood N Wine Factorio Edition Linux Server/data/base/scenarios/rocket-rush/control.lua
+++
@@ -1,4 +0,0 @@
-local handler = require("event_handler")
-
-handler.add_lib(require("silo-script"))
-handler.add_lib(require("rocket-rush"))
--- Blood N Wine Factorio Edition Linux Server/data/base/scenarios/team-production/control.lua
+++
@@ -1,5 +0,0 @@
-local handler = require("event_handler")
-
-local team_production = require "team_production"
-
-handler.add_lib(team_production)
